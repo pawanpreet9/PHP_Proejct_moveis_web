4477370ddae0ff4f294c715f619e12f5d4cd7992
Pawanpreet9 <126494380+Pawanpreet9@users.noreply.github.com>
2023-03-29 22:22:51 -0700

    Update connect.php
    addition of viarables

--- connect.php
+++ connect.php
@@ -1,14 +1,20 @@
  <?php
-     define('DB_DSN','mysql:host=localhost;dbname=serverside;charset=utf8');
-     define('DB_USER','serveruser');
-     define('DB_PASS','gorgonzola7!');     
-     
+    // define('DB_DSN','mysql:host=localhost;dbname=serverside;charset=utf8');
+    // define('DB_USER','serveruser');
+   //  define('DB_PASS','gorgonzola7!');     
+       defined('BASEPATH');
+     $host = 'localhost';
+     $user = 'serveruser';
+     $password = 'gorgonzola7!';
+     $dbname = 'serverside';
+     $dsn = '';  
     //  PDO is PHP Data Objects
     //  mysqli <-- BAD. 
     //  PDO <-- GOOD.
      try {
+     $dsn = 'mysql:host='.$host.'$dbname='.$dbname;
          // Try creating new PDO connection to MySQL.
-         $db = new PDO(DB_DSN, DB_USER, DB_PASS);
+         $db = new PDO($dsn,$user,$password);
          //,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
      } catch (PDOException $e) {
          print "Error: " . $e->getMessage();
