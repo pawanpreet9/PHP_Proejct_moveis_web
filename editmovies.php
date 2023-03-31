6c7566db29428bc93f81f21e7c80f9412fa918d7
Pawanpreet9 <126494380+Pawanpreet9@users.noreply.github.com>
2023-03-30 21:39:58 -0700

    Update editmovies.php
    Addition of upload image code

--- editmovies.php
+++ editmovies.php
@@ -88,6 +88,43 @@ require('authenticate.php');
 function confirm(){
     return confirm("Are you sure to delete this post!!");
 }
+ // file_upload_path() - Safely build a path String that uses slashes appropriate for our OS.
+    // Default upload path is an 'uploads' sub-folder in the current folder.
+    function file_upload_path($original_filename, $upload_subfolder_name = 'uploads') {
+       $current_folder = dirname(__FILE__);
+       
+       // Build an array of paths segment names to be joins using OS specific slashes.
+       $path_segments = [$current_folder, $upload_subfolder_name, basename($original_filename)];
+       
+       // The DIRECTORY_SEPARATOR constant is OS specific.
+       return join(DIRECTORY_SEPARATOR, $path_segments);
+    }
+
+    // file_is_an_image() - Checks the mime-type & extension of the uploaded file for "image-ness".
+    function file_is_an_image($temporary_path, $new_path) {
+        $allowed_mime_types      = ['image/gif', 'image/jpeg', 'image/png','image/pdf'];
+        $allowed_file_extensions = ['gif', 'jpg', 'jpeg', 'png','pdf'];
+        
+        $actual_file_extension   = pathinfo($new_path, PATHINFO_EXTENSION);
+        $actual_mime_type        = getimagesize($temporary_path)['mime'];
+        
+        $file_extension_is_valid = in_array($actual_file_extension, $allowed_file_extensions);
+        $mime_type_is_valid      = in_array($actual_mime_type, $allowed_mime_types);
+        
+        return $file_extension_is_valid && $mime_type_is_valid;
+    }
+    
+    $image_upload_detected = isset($_FILES['image']) && ($_FILES['image']['error'] === 0);
+    $upload_error_detected = isset($_FILES['image']) && ($_FILES['image']['error'] > 0);
+
+    if ($image_upload_detected) { 
+        $image_filename        = $_FILES['image']['name'];
+        $temporary_image_path  = $_FILES['image']['tmp_name'];
+        $new_image_path        = file_upload_path($image_filename);
+        if (file_is_an_image($temporary_image_path, $new_image_path)) {
+            move_uploaded_file($temporary_image_path, $new_image_path);
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -137,7 +174,16 @@ function confirm(){
                         <p>
                             <label for="price">Price</label>
                             <input type="text" name="price" value="<?= $row['price'] ?>" id="price">
-                        </p>
+                        </p> <p>
+                          Image : <?= $row['image'] ?>
+                            
+                        </p>         
+                        <p>
+                                        
+                       
+         <label for='image'>Change Image:</label>
+         <input type='file' name='image' id='image'>
+         </p>
 
 
                         <input type="hidden" name="id" value="<?= $row['movie_id'] ?>">
@@ -147,6 +193,11 @@ function confirm(){
                 </fieldset>
 
             </form>
+                 <form method='post' enctype='multipart/form-data'>
+         <label for='image'>Image Filename:</label>
+         <input type='file' name='image' id='image'>
+         <input type='submit' name='submit' value='Upload Image'>
+     </form>
             <?php if((empty($_POST['name']) || empty($_POST['description']))&&isset($_POST['button1'])): ?>
             <p id="error"><b>An error ouccured while proceding</b><br>**There should be at least on character in title and content to update the given post**</p>
             <a href="admin.php">Return to home</a>
