3b463a3d30530977602e13693fe626d58421a74a
Pawanpreet9 <126494380+Pawanpreet9@users.noreply.github.com>
2023-04-05 08:49:26 -0700

    Update addmovies.php
    addition of new code

--- addmovies.php
+++ addmovies.php
@@ -9,8 +9,9 @@
 ****************/
 
 require('connect.php');
-require('authenticate.php');
-    if ($_POST && !empty($_POST['name']) && !empty($_POST['description'] && !empty($_POST['language']))) {
+
+
+    if ( !empty($_POST['name']) && !empty($_POST['description'] && !empty($_POST['language']))) {
         //  Sanitize user input to escape HTML entities and filter out dangerous characters.
         $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -18,9 +19,10 @@ require('authenticate.php');
         $actors = filter_input(INPUT_POST,'actors',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $directors = filter_input(INPUT_POST,'directors',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $price = filter_input(INPUT_POST,'price',FILTER_SANITIZE_NUMBER_INT);
+        $image = $_FILES['image']['name'];
         
         //  Build the parameterized SQL query and bind to the above sanitized values.
-            $query = "INSERT INTO movies (movie_name,description,language,actor_names,director_names,price) VALUES (:name,:description,:language,:actors,:directors,:price)";
+            $query = "INSERT INTO movies (movie_name,description,language,actor_names,director_names,price,image) VALUES (:name,:description,:language,:actors,:directors,:price,:image)";
             $statement = $db->prepare($query);
         
         //  Bind values to the parameters
@@ -30,6 +32,7 @@ require('authenticate.php');
            $statement->bindValue(':actors',$actors);
            $statement->bindValue(':directors',$directors);
            $statement->bindValue(':price',$price);
+           $statement->bindValue(':image',$image);
         
         //  Execute the INSERT.
         //  execute() will check for possible SQL injection and remove if necessary
@@ -39,9 +42,49 @@ require('authenticate.php');
             header("Location: admin.php");
            }
 
+}
+    
 
+
+
+
+    // file_upload_path() - Safely build a path String that uses slashes appropriate for our OS.
+    // Default upload path is an 'uploads' sub-folder in the current folder.
+    function file_upload_path($original_filename, $upload_subfolder_name = 'uploads') {
+       $current_folder = dirname(__FILE__);
+       
+       // Build an array of paths segment names to be joins using OS specific slashes.
+       $path_segments = [$current_folder, $upload_subfolder_name, basename($original_filename)];
+       
+       // The DIRECTORY_SEPARATOR constant is OS specific.
+       return join(DIRECTORY_SEPARATOR, $path_segments);
     }
 
+    // file_is_an_image() - Checks the mime-type & extension of the uploaded file for "image-ness".
+    function file_is_an_image($temporary_path, $new_path) {
+        $allowed_mime_types      = ['image/gif', 'image/jpeg', 'image/png','image/pdf'];
+        $allowed_file_extensions = ['gif', 'jpg', 'jpeg', 'png','pdf'];
+        
+        $actual_file_extension   = pathinfo($new_path, PATHINFO_EXTENSION);
+        $actual_mime_type        = getimagesize($temporary_path)['mime'];
+        
+        $file_extension_is_valid = in_array($actual_file_extension, $allowed_file_extensions);
+        $mime_type_is_valid      = in_array($actual_mime_type, $allowed_mime_types);
+        
+        return $file_extension_is_valid && $mime_type_is_valid;
+    }
+    
+    $image_upload_detected = isset($_FILES['image']) && ($_FILES['image']['error'] === 0);
+    $upload_error_detected = isset($_FILES['image']) && ($_FILES['image']['error'] > 0);
+
+    if ($image_upload_detected) { 
+        $image_filename        = $_FILES['image']['name'];
+        $temporary_image_path  = $_FILES['image']['tmp_name'];
+        $new_image_path        = file_upload_path($image_filename);
+        if (file_is_an_image($temporary_image_path, $new_image_path)) {
+            move_uploaded_file($temporary_image_path, $new_image_path);
+        }
+    }
 
 ?>
 
@@ -63,42 +106,58 @@ require('authenticate.php');
         
         <ul id="menu">
             <li><a href="index.php">Home</a></li>
-            <li><a href="admin.php" class="active">Admin</a></li>
+            <li><a href="viewMovies.php" class="active">View Movies</a></li>
+            <li><a href="admin.php">Admin</a></li>
 
         </ul>
     </div>
         <div>
-            <form action="addmovies.php" method="post">
+            <form action="addmovies.php" method="post" enctype='multipart/form-data'>
                 <fieldset>
                     <legend>New Movie Information</legend>
                     <p>
                         <label for="name">Movie Name</label>
-                        <input type="text" name="name" id="name">
+                        <input required = "required" type="text" name="name" id="name">
                     </p>
                     <p>
                         <label for="description">Description</label>
-                        <textarea name="description" id="description"></textarea>
+                        <textarea required = "required" name="description" id="description"></textarea>
                     </p>
                     <p>
                         <label for="language">Language</label>
-                        <textarea name="language" id="language"></textarea>
+                        <textarea required = "required" name="language" id="language"></textarea>
                     </p>
                     <p>
                         <label for="actors">Actors</label>
-                        <textarea name="actors" id="actors"></textarea>
+                        <textarea required = "required" name="actors" id="actors"></textarea>
                     </p>
                     <p>
                         <label for="directors">Directed By</label>
-                        <textarea name="directors" id="directors"></textarea>
+                        <textarea required = "required" name="directors" id="directors"></textarea>
                     </p>
                     <p>
                         <label for="price">Price</label>
-                        <textarea name="price" id="price"></textarea>
+                        <textarea required = "required" name="price" id="price"></textarea>
                     </p>
-                    <button name="button" type="submit">Create</button>
+                 <p>
+                        
+
+                        
+                        
+         <label for='image'>Image Filename:</label>
+         <input type='file' name='image' id='image'>
+             
+     </p>
+
+            
+                             <button name="button" type="submit">Create</button>
                 </fieldset>
             </form>
-            <?php if(isset($_POST['button']) && (empty($_POST['name']) || empty($_POST['description']) || empty($_POST['language']))): ?>
+                    
+      
+
+           
+            <?php if(isset($_POST['button']) && (empty($_POST['name']) || empty($_POST['description']))): ?>
                 <p id="error">**There should be at least one character in movie name and description**</p>
             
         <?php endif ?>
@@ -107,6 +166,7 @@ require('authenticate.php');
     
     <div id="footer">
         <a href="index.php"><pre>Home</pre></a>
+            <a href="viewMovies.php"><pre>View Movies</pre></a>
             <a href="admin.php"><pre>Admin</pre></a>
     </div>
 </div>
