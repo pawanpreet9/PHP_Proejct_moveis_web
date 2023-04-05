5d94d8ab402ff680bda897011b4340346af3ffd2
Pawanpreet9 <126494380+Pawanpreet9@users.noreply.github.com>
2023-04-05 08:51:37 -0700

    Update registered.php
    change the table

--- registered.php
+++ registered.php
@@ -17,7 +17,7 @@ require 'connect.php'; //require connection script
          $password = password_hash($password, PASSWORD_BCRYPT, array("cost" => 12));
           
          //Check if username exists
-         $query = "SELECT COUNT(user_name) AS user FROM admin WHERE user_name = :username";
+         $query = "SELECT COUNT(user_name) AS user FROM users WHERE user_name = :username";
          $statement = $db->prepare($query);
 
          $statement->bindValue(':username', $username);
@@ -30,7 +30,7 @@ require 'connect.php'; //require connection script
         
        else{
 
-    $statement = $dsn->prepare("INSERT INTO admin (user_name, email, password) 
+    $statement = $dsn->prepare("INSERT INTO users (user_name, email, password) 
     VALUES (:username,:email, :password)");
     $statement->bindParam(':username', $username);
     $statement->bindParam(':email', $email);
@@ -54,10 +54,53 @@ require 'connect.php'; //require connection script
      }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Blockbuster Movies - Registeration</title>
+</head>
+<body>
+    <!-- Remember that alternative syntax is good and html inside php is bad -->
+    
+        <div id="header">
+            <h1><a href="index.php">Blockbuster Movies</a></h1>
+        
+        <ul id="menu">
+            <li><a href="index.php" class="active">Home</a></li>
+            <li><a href="viewMovies.php">View Movies</a></li>
+            <li><a href="login.php">login</a></li>
+            
+        </ul>
+        </div>
 
 <form action="registered.php" method="post">
-  <input type="text" required="required" name="username" placeholder="Username">
-  <input required="required" type="email" name="email" placeholder="Email">
-  <input required="required" type="password" name="password" placeholder="Password">                  
+    <ul>
+        <li>
+    <label>Enter User Name</label>
+  <input type="text" required="required" name="username" >
+</li>
+<li>
+  <label>Enter Email ID</label>
+  <input required="required" type="email" name="email" placeholder="ex: someone@gmail.com">
+</li>
+<li>
+  <label>Enter Password</label>
+  <input required="required" type="password" name="password"> 
+  </li>
+  <li>                 
   <button name="submit" type="submit">register</button>
+</li>
+</ul>
   </form>
+  <div id="footer">
+            <a href="index.php"><pre>Home</pre></a>
+            <a href="viewMovies.php"><pre>View Movies</pre></a>
+            <a href="authenticate.php"><pre>LogIn</pre></a>
+        </div>
+    </body>
+    </html>
+
